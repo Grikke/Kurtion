50e6d2577e1726924e658f5c89c62ae76b10a25b
Arnaud-Léandre VOISIN TRIBOULET <arnaud-leandre.voisin-triboulet@platypus-agency.fr>
2021-07-24 11:15:22 +0200

    New object prototypes

--- kurtion.js
+++ kurtion.js
@@ -3,6 +3,18 @@ Object.prototype.forEach = function(func) {
     func(key, this[key])
   })
 }
+Object.prototype.count = function() {
+  return Object.keys(this).length
+}
+Object.prototype.filter = function(func) {
+  let obj = this
+  Object.keys(this).filter(key => {
+    if (!(func(key, this[key]))) {
+      delete obj[key]
+    }
+  })
+  return obj
+}
 String.prototype.ucfirst = function() {
   return `${this.charAt(0).toUpperCase()}${this.slice(1)}`
 }
